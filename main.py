ddce8ae2366e45723d4f9f028ac2f7135584058c
hgrahamcs <hgrahamcs@gmail.com>
2020-07-10 19:53:21 -0500

    Added documentation around who developed this, it's use, and what certain areas do

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
+# Written by Henry Graham for the purpose of analyzing the contents of my .mbox files.
+# Feel free to use the below code in any way possible without the need to release the end product.
+
 import mailbox
 import sys
 import re
 
 
 def get_from(mboxfile, fileout):
-    locallist = []
+    locallist = [] # List in which from lines will be held.
+
     mbox = mailbox.mbox(mboxfile)
+
+    # For each message in the mailbox look at the from line and put into the list.
     for message in mbox:
-        frm = message['from']
+        frm = message['From']
         locallist.append(frm)
     
     for x in locallist:
-        match = re.findall(r'[\w\.-]+@[\w\.-]+', x) # 90% sure this regex catches everything, in my test data it caught every email
+
+        # This try/except is to catch errors pertaining to spam. Some spammers do weird stuff with addresses and names which results in an error
+        # being thrown here. I'll review it if it is encountered more
+        try:
+            match = re.findall(r'[\w\.-]+@[\w\.-]+', x) # 90% sure this regex catches everything, in my test data it caught every email
+        except:
+            print(x)
 
         # Yes this is not the ideal way of doing this, I'll fix it if it creates issues later on.
+        # The try except is also a result of spam. Similar reason with outputs. Can be resolved by fixing the Header issue encountered in DS1.
         try:
             fileout.writelines(match)
             fileout.write("\n")
         except:
             print(x)
 
-
 if __name__ == "__main__":
-
-    frmfile = open(sys.argv[1]+ "frmlist.txt", "w")
-
+    
+    # Getting the path to the .mbox file and creating the .txt file in the same dir as the mbox file
+    frmfile = open(sys.argv[1]+ ".fromlist.txt", "w")
     get_from(sys.argv[1], frmfile)
 
     frmfile.close()
